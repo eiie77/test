2d25aade2bce2eb2201f53fed7ab542f12929c2d
kimin kyeong <kimin kyeong@121.170.150.61>
2020-08-11 15:32:16 +0900

    first commit
    첫번째 커밋

---
+++ KkmProject/src/main/java/com/kkm/kkmProject/KkmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kkm.kkmProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkmProjectApplication.class, args);
+	}
+
+}
---
+++ KkmProject/src/test/java/com/kkm/kkmProject/KkmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkm.kkmProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkmProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
