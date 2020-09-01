841404d2049e0f80f66df1c45ff417e5c86a9df1
nulls <nulls@DESKTOP-BC4RFJ4>
2020-09-01 09:51:16 +0900

    3.세번째 커밋

--- siwasoft/src/main/java/com/siwasoft/softmanager/HomeController.java
+++ siwasoft/src/main/java/com/siwasoft/softmanager/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		dd
+		System.out.println("Á¦¹ß");
 		return "home";
 	}
 	
